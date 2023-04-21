968b2fcecfabb08cf7a68aeb69226b0cf266fd49
vanchhita <vanchhitarai2000@gmail.com>
2023-04-21 12:04:43 +0530

    final edit

--- deploy.py
+++ deploy.py
@@ -106,7 +106,7 @@ print(artifact_path)
 
 #Download the artifact in local machine
 
-model_path = mlflow.artifacts.download_artifacts(artifact_path,dst_path="C:\\Users\shrisgupta\Desktop\Models")
+model_path = mlflow.artifacts.download_artifacts(artifact_path,dst_path="C:\Users\v\Desktop\Models")
 
 print("MODEL PATH:",model_path)
 
